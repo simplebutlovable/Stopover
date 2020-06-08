8e2a6d12f040f3753e65801ea69ef77302fa7381
“simplebutlovable” <jgenz27@gmail.com>
2020-06-08 22:12:12 +0800

    Test Commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Search (models.Model):
+    search = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.search
+
+    class Meta:
+        verbose_name_plural = "Searches"
