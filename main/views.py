04cff0a6c6151291cacd7c04769cecde16a932c0
“simplebutlovable” <jgenz27@gmail.com>
2020-06-08 23:29:13 +0800

    June 8, 2020 10:40 PM

--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Search
 
 def index(request):
-    return render(request, 'base.html')
+    if request.method == 'GET':
+        return render(request, 'base.html')
+
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        print(search)
+        Search.objects.create(search=search)
+        return render(request,'base.html')
+
+
